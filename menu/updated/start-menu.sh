9a8c586b408d270a33def749d26ce541cdfb3d46
Aryo brokolly <98819851+aryobrokolly@users.noreply.github.com>
2023-06-01 01:06:17 +0700

    Update start-menu.sh

--- menu/updated/start-menu.sh
+++ menu/updated/start-menu.sh
@@ -45,7 +45,7 @@ clear
 # echo -e "$BLUE━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"$NC
 # figlet LIZSVR | lolcat
 # #echo -e "$NC"
-# echo -e "Telegram : @liz_mine"
+# echo -e "Telegram : @Opindoo"
 echo -e "$BLUE━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$NC"
 echo -e "$BRED          Informasi System           $NC"
 echo -e "$BLUE━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$NC"
@@ -130,5 +130,5 @@ fi
 # echo -e " Fail2ban                :$RED [Error] $NC"
 # fi
 echo -e "$BLUE━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$NC"
-echo -e "$BRED----------- Mod By LIZSVR -----------"
+echo -e "$BRED------- Mod By ARYO BROKOLLY --------"
 echo -e "$BLUE━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$NC"
