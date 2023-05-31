30ab2332428e598b71c94733ae5ad29194f8b279
Aryo brokolly <98819851+aryobrokolly@users.noreply.github.com>
2023-06-01 01:07:40 +0700

    Update mbackup.sh

--- menu/updated/mbackup.sh
+++ menu/updated/mbackup.sh
@@ -19,7 +19,7 @@ echo -e "${NC}${GREEN}Permission Accepted...${NC}"
 else
 echo -e "${NC}${RED}Permission Denied!${NC}";
 echo -e "${NC}${LIGHT}Please Contact Admin!!"
-echo -e "${NC}${LIGHT}Telegram : https://t.me/liz_mine"
+echo -e "${NC}${LIGHT}Telegram : https://t.me/Opindoo"
 exit 0
 fi
 clear
